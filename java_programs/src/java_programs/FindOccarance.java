9655030e586127cd783f4dda0f5abf9a8d65bb80
rajratn more <rajmorecsn@gmail.com>
2022-03-30 10:39:23 +0530

    file created on March30_2022

---
+++ java_programs/src/java_programs/FindOccarance.java
@@ -0,0 +1,39 @@
+package java_programs;
+import java.util.*;
+
+public class FindOccarance {
+
+	static int first = -1, last = -1;
+	static boolean flag = true;
+	
+	static void findOccarance(String str,char ch,int index) {
+		if(index == str.length())
+			return;
+		
+		if(str.charAt(index) == ch) {
+			if(first == -1) {
+				first = index;
+			}else {
+				last = index;
+			}
+		}
+		findOccarance(str,ch,index+1);
+		if(flag) {
+			System.out.println("first and last occarance is "+first+" "+last);
+			flag=false;
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter String");
+		String str = sc.nextLine();
+		System.out.println("Find character");
+		char ch = sc.next().charAt(0);
+		findOccarance(str,ch,0);
+		sc.close();
+
+	}
+
+}
