0a37a1342678f3ba6dd80349f21399ae29707079
rajratn more <rajmorecsn@gmail.com>
2022-04-07 14:54:11 +0530

    edited

--- java_programs/src/java_programs/CopyArray.java
+++ java_programs/src/java_programs/CopyArray.java
@@ -1,25 +1,35 @@
 package java_programs;
+import java.util.*;
 
 public class CopyArray {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
-		int arr[] = new int[] {12,35,65,28,66};
-		int arr1[] = new int[arr.length];
+		List<Integer> arr1 = new ArrayList<>();
+		List<Integer> arr = new ArrayList<>();
 		
-		for(int i=0; i<arr.length; i++) {
-			arr1[i] = arr[i];
-		}
-
-		System.out.println("\n Actual Array");
-		for(int i=0; i<arr.length; i++) {
-			System.out.print(" "+arr[i]);
+		System.out.println("enter array of 5");		
+		for(int i=0; i<5; i++) {
+			arr.add(sc.nextInt());
 		}
 		
-		System.out.println("\n Copied Array");
-		for(int i=0; i<arr1.length; i++) {
-			System.out.print(" "+arr1[i]);
-		}
+		System.out.println(arr);
+		
+		arr1.addAll(arr);
+		
+		System.out.println(arr1);
+		
+//
+//		System.out.println("\n Actual Array");
+//		for(int i=0; i<arr.length; i++) {
+//			System.out.print(" "+arr[i]);
+//		}
+//		
+//		System.out.println("\n Copied Array");
+//		for(int i=0; i<arr1.size(); i++) {
+//			System.out.print(" "+arr1.get(i));
+//		}
 	}
 
 }
