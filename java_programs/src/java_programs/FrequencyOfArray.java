51cf963d51d2a7a732ad55131befcfaa866839d1
rajratn more <rajmorecsn@gmail.com>
2022-04-07 14:54:00 +0530

    created on April_7

---
+++ java_programs/src/java_programs/FrequencyOfArray.java
@@ -0,0 +1,26 @@
+package java_programs;
+import java.util.*;
+
+public class FrequencyOfArray {
+
+	public static void main(String[] args) {
+		
+		int arr[] = new int[] {1,2,8,3,2,2,2,5,1};
+		Map<Integer,Integer> mp = new HashMap<>();
+		
+		for(int i=0; i<arr.length; i++) {
+			mp.put(arr[i],0);
+		}
+//		System.out.println(mp);
+		
+		for(int i=0; i<arr.length; i++) {
+			for(Map.Entry<Integer, Integer> e : mp.entrySet()) {
+				if(arr[i] == e.getKey()) {
+					mp.put(arr[i], e.getValue()+1);
+				}
+			}
+		}
+		System.out.println(mp);
+	}
+
+}
