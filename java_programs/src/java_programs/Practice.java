0d6851d2bdf994408b898ac98873d79894b2b458
rajratn more <rajmorecsn@gmail.com>
2022-04-07 16:19:02 +0530

    edited

--- java_programs/src/java_programs/Practice.java
+++ java_programs/src/java_programs/Practice.java
@@ -2,7 +2,10 @@ package java_programs;
 public class Practice {	
 	
 	public static void main(String[] args) {
+		int arr[] = new int[5];
 		
+		for(int i=0; i<arr.length; i++)
+			System.out.println(arr[i]);
 	
 	}
 }
