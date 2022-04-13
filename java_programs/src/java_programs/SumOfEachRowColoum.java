d251f2cd64bd6a7ce41e49ab066d62029c680fe1
rajratn more <rajmorecsn@gmail.com>
2022-04-13 19:29:39 +0530

    created

--- java_programs/src/java_programs/SumOfEachRowColoum.java
+++ java_programs/src/java_programs/SumOfEachRowColoum.java
@@ -3,8 +3,27 @@ package java_programs;
 public class SumOfEachRowColoum {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int arr[][] = new int[][] {
+			{1,2,3},
+			{4,5,6},
+			{7,8,9}
+		};
+		
+		int first=0;
+		int second=0;
+		int third=0;
+		for(int i=0; i<arr.length; i++) {
+			int rowCount=0;
+			int columnCount=0;
+			for(int j=0; j<arr[0].length; j++) {
+				columnCount+=arr[j][i];
+				rowCount+=arr[i][j];				
+			}
+			System.out.println(" row count is "+rowCount);
+			System.out.println(" column count is "+columnCount);
+		}
+		
 	}
 
 }
